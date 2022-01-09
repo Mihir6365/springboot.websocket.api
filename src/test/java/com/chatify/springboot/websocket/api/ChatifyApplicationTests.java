d75353a30ebe60cf103c979aaac09398d74afdce
Mihir <savla.mp@somaiya.edu>
2022-01-09 12:55:45 +0530

    First trial

---
+++ src/test/java/com/chatify/springboot/websocket/api/ChatifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatify.springboot.websocket.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
